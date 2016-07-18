f8b5cbd4f979239c1129185980cbea17a889b66f
janakneissl <jany95@outlook.com>
2016-07-18 18:34:32 +0900

    Update coverage.py
    import re
    
    rep = {"condition1": "", "condition2": "text"} # define desired replacements here
    
    # use these three lines to do the replacement
    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
    pattern = re.compile("|".join(rep.keys()))
    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)

--- coverage.py
+++ coverage.py
@@ -12,6 +12,7 @@
 
 
 import pysam
+import re
 
 #the discordant read bam file is read in 
 samfile = pysam.AlignmentFile("/home/jana/j_data/201606_Transloci/01_bam_file/discordant.bam", "rb")
@@ -19,6 +20,9 @@ samfile = pysam.AlignmentFile("/home/jana/j_data/201606_Transloci/01_bam_file/di
 chromosomes = ['1','2','3','4','5','6','7','8','9','10','11', '12', '13', '14', '15', 
 				'16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M']
 
+rep = {'X':'23', 'Y':'24', 'M':'25'}
+rep = dict(rep.iteritems())
+pattern = re.compile("|".join(rep.keys()))
 
 for i in chromosomes: #for each chromosome the coverage information is saved in its own file
 	chr = "chr" + i
@@ -26,10 +30,11 @@ for i in chromosomes: #for each chromosome the coverage information is saved in 
     f = open(filename,'w')
 	for column in samfile.pileup(chr): #Loops through every position of the current chromosome
 		#then get the information for all the reads at this position and their mate reads
+		i = pattern.sub(lambda m:rep[re.escape(m.group(0))],i)
 		fst_pos = column.pos
 		for read in column.pileups:
 			mate_chr = read.alignment.next_reference_name.split('')[3]
-			mate_pos = read.alignemt.next_reference_start
+			mate_pos = pattern.sub(lambda m: rep[re.escape(m.group(0))],read.alignemt.next_reference_start)
 			seq =(i, fst_pos, mate_chr, mate_pos)
 			f.write('\t'.join(seq))
 			f.write('\n')
