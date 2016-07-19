cbc7771ae81d43aaaa6a076ddab185fee74b5a83
janakneissl <jany95@outlook.com>
2016-07-19 13:00:09 +0900

    Update dup_reorga.py

--- dup_reorga.py
+++ dup_reorga.py
@@ -4,6 +4,8 @@
 import numpy as np
 import re
 import pandas as pd
+
+#Set up re dictionnary to replace chrX|Y|M with numbers 23,24,25
 rep = {'X':'23', 'Y':'24', 'M':'25', 'chr':''}
 rep = dict(rep.iteritems())
 pattern = re.compile("|".join(rep.keys()))
@@ -11,6 +13,7 @@ pattern = re.compile("|".join(rep.keys()))
 
 outfile = open('test_File.txt', 'w')
 
+#open original Seg_Dup-File and have each region printed to a new line
 with open("Annotate_SegDups.txt", "r") as SegDup:
   for line in SegDup:
     line = pattern.sub(lambda m:rep[re.escape(m.group(0))],line)
@@ -24,10 +27,11 @@ with open("Annotate_SegDups.txt", "r") as SegDup:
       
 outfile.close()
 
+#Drop duplicate rows and sort by chromosome
 dup_df = pd.read_csv('test_File.txt', sep='\t', header=None, index_col =None, dtype = int)
 dup_df = pd.dup_df.drop_duplicates()
 dup_df.columns = ['chr', 'start', 'end']
-dup_df.sort_values(['chr', 'start'])
+dup_df = dup_df.sort_values(['chr', 'start'])
 
-pd.dup_df.to_csv('Seg_Dup_orga.txt',sep='\t')
+pd.dup_df.to_csv('Seg_Dup_orga.txt',sep='\t', header = False, index=False)
       
