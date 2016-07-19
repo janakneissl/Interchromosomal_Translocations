9e315910b53a9135402a5311a02c2d8aed976839
janakneissl <jany95@outlook.com>
2016-07-19 13:20:12 +0900

    Update repeat_reorga.py

--- repeat_reorga.py
+++ repeat_reorga.py
@@ -2,7 +2,18 @@
 
 import numpy as np
 import re
+rep = {'X':'23', 'Y':'24', 'M':'25', 'chr':''}
+rep = dict(rep.iteritems())
+pattern = re.compile("|".join(rep.keys()))
 
+
+outfile = open('Repeats_orga.txt','w')
 with open("Annotate_Repeats.txt","r") as repeats:
   for line in repeats:
+    line = pattern.sub(lambda m:rep[re.escape(m.group(0))],line)
+    line = re.split('\t', line.replace('\n', ''))
+    outfile.write('\t'.join(line[:3])
+    
+outfile.close()
+    
     
